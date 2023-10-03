1dbf8e212c6f9a4ac43a6f23256390c36fe0a98a
BrnCalisario <bruno145224@gmail.com>
2023-10-03 16:31:32 -0300

    add: home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,19 @@
+function Home() {
+  return (
+    <div>
+        
+        <div id='my-name'>
+            <h1>BrnCalisario</h1>
+        </div>
+
+        <div className='side-text'>
+            <p>Oi eu sou oi eu sou Oi eu sou oi eu souOi eu sou oi eu souOi 
+                eu sou oi eu souOi eu sou oi eu souOi eu sou oi eu souOi 
+                eu sou oi eu souOi eu sou oi eu souOi eu sou oi eu sou</p>
+        </div>
+
+    </div>
+  )
+}
+
+export default Home
