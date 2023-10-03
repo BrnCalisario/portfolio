d36aa1abf5c9bd97a124688f7d0308bf29948d15
BrnCalisario <bruno145224@gmail.com>
2023-10-03 16:59:45 -0300

    add: routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes } from 'react-router-dom';
 
 import './App.css';
 import Header from './components/Header';
+import Home from './pages/Home';
 import ThemeProvider from './providers/ThemeContextProvider';
 
 function App() {
@@ -9,6 +11,9 @@ function App() {
 		<ThemeProvider>
 			<div className="App">
 				<Header />
+				<Routes>
+					<Route path='/' element={<Home />} />
+				</Routes>
 			</div>
 		</ThemeProvider>
 	);
