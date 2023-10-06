f77abe44b5efe19a86fb6d6f7ed8f10b3d9cfc58
BrnCalisario <bruno145224@gmail.com>
2023-10-06 11:36:13 -0300

    add: prettier config

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,35 +1,36 @@
-import { Nav, NavItem, Navbar, Button } from "react-bootstrap"
-import { NavLink } from "react-router-dom"
-import { useThemeContext } from "../../contexts/ThemeContext"
-import ThemeToggle from "../ThemeToggle"
+import { Nav, NavItem, Navbar, Button } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+import { useThemeContext } from '../../contexts/ThemeContext';
+import ThemeToggle from '../ThemeToggle';
 
-import "./style.scss"
+import './style.scss';
 
 function Header() {
+	const { theme } = useThemeContext();
 
-    const { theme } = useThemeContext()
-
-    return (
-        <Navbar expand="lg" className="bg-body-tertiary" id="header"
-            data-bs-theme={theme}>
-
-            <Navbar.Brand>BrnCalisario</Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                    <NavItem>
-                        <NavLink className="nav-link" to={""}>Home</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink className="nav-link" to={""}>Sobre mim</NavLink>
-                    </NavItem>
-                </Nav>
-                <Nav>
-                    <ThemeToggle />
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
-    )
+	return (
+		<Navbar expand='lg' className='bg-body-tertiary' id='header'>
+			<Navbar.Brand>BrnCalisario</Navbar.Brand>
+			<Navbar.Toggle aria-controls='responsive-navbar-nav' />
+			<Navbar.Collapse id='basic-navbar-nav'>
+				<Nav className='me-auto'>
+					<NavItem>
+						<NavLink className='nav-link' to={''}>
+							Home
+						</NavLink>
+					</NavItem>
+					<NavItem>
+						<NavLink className='nav-link' to={''}>
+							Sobre mim
+						</NavLink>
+					</NavItem>
+				</Nav>
+				<Nav>
+					<ThemeToggle />
+				</Nav>
+			</Navbar.Collapse>
+		</Navbar>
+	);
 }
 
-export default Header
+export default Header;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,12 @@
 function Home() {
-  return (
-    <div>
-        
-        <div id='my-name'>
-            <h1>BrnCalisario</h1>
-            <p>Software Developer</p>
-        </div>
-
-    </div>
-  )
+	return (
+		<div>
+			<div id='my-name'>
+				<h1>BrnCalisario</h1>
+				<p>Software Developer</p>
+			</div>
+		</div>
+	);
 }
 
-export default Home
+export default Home;
