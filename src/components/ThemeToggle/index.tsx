1c6e5ce53bf0b7167febd579f1b37e5243bf3662
BrnCalisario <bruno145224@gmail.com>
2023-10-09 10:24:18 -0300

    add: home media query

--- src/components/ThemeToggle/index.tsx
+++ src/components/ThemeToggle/index.tsx
@@ -1,19 +1,22 @@
-import "./style.scss"
+import './style.scss';
 
-import { Button } from 'react-bootstrap'
+import { Button } from 'react-bootstrap';
 
-import { FaSun, FaRegMoon } from "react-icons/fa";
+import { FaSun, FaRegMoon } from 'react-icons/fa';
 import { useThemeContext } from '../../contexts/ThemeContext';
 
 function ThemeToggle() {
-    
-    const  { theme, toggleTheme, isDark } = useThemeContext()
+	const { toggleTheme, isDark } = useThemeContext();
 
-    return (
-        <button id="btn" className={ isDark ? 'light' : 'dark' } onClick={toggleTheme}>
-            { isDark ? <FaSun /> : <FaRegMoon /> } 
-        </button>
-    )
+	return (
+		<button
+			id='btn'
+			className={isDark ? 'light' : 'dark'}
+			onClick={toggleTheme}
+		>
+			{isDark ? <FaSun /> : <FaRegMoon />}
+		</button>
+	);
 }
 
-export default ThemeToggle
+export default ThemeToggle;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,17 @@
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import './index.scss';
+
 function Home() {
 	return (
-		<div>
-			<div id='my-name'>
-				<h1>BrnCalisario</h1>
+		<div id='home'>
+			<div id='brand'>
+				<div className='wrap-links'>
+					<h1>BrnCalisario</h1>
+					<div className='links d-flex gap-4'>
+						<FaGithub />
+						<FaLinkedin />
+					</div>
+				</div>
 				<p>Software Developer</p>
 			</div>
 		</div>
